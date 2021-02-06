06e8a32bbf2ccb80c27d0cc4edc1c0156955cc6c
Akinwale Habib <akinwalehabib@hotmail.com>
2021-02-07 00:32:48 +0100

    ch: fix empty object issue in mongoose

--- server/events/expenseEvent.js
+++ server/events/expenseEvent.js
@@ -3,11 +3,14 @@ const mongoose = require('mongoose');
 const Model = mongoose.model;
 const Schema = mongoose.Schema;
 
-const expenseEventSchema = new Schema({
-    type: { type: String, default: ""},
-    expenseId: { type: String, default: ""},
-    timestamp: { type: Date, default: Date.now },
-    data: { type: Schema.Types.Mixed, default: {}}
-});
+const expenseEventSchema = new Schema(
+    {
+        expenseId: { type: String, default: ""},
+        type: { type: String, default: ""},
+        timestamp: { type: Date, default: Date.now },
+        data: { type: Schema.Types.Mixed, default: {} },
+    }, 
+    { retainKeyOrder: true, minimize: false }
+);
 
 module.exports = Model('ExpenseEvent', expenseEventSchema);
--- server/models/expense.js
+++ server/models/expense.js
@@ -3,10 +3,13 @@ const mongoose = require('mongoose');
 const Model = mongoose.model;
 const Schema = mongoose.Schema;
 
-const expenseSchema = new Schema({
-    id: { type: String, default: ""},
-    description: { type: String, default: ""},
-    amount: { type: Schema.Types.Decimal128, default: 0.0}
-});
+const expenseSchema = new Schema(
+    {
+        expenseId: {type: String, default: ""},
+        description: { type: String, default: ""},
+        amount: { type: Schema.Types.Decimal128, default: 0.0}
+    },
+    { retainKeyOrder: true, minimize: false }
+);
 
 module.exports = Model('Expense', expenseSchema);
