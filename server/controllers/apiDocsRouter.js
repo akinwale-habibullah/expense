0d18aee68f2df3b6f8049fc130fb80ae81e44244
Akinwale Habib <akinwalehabib@hotmail.com>
2021-02-07 15:35:14 +0100

    ch: refactor OAS and use YAML

--- server/controllers/apiDocsRouter.js
+++ server/controllers/apiDocsRouter.js
@@ -1,11 +1,24 @@
+const fs = require('fs');
+const path = require('path');
 const express = require('express');
 const redoc = require('redoc-express');
+const yaml = require('js-yaml');
 
 const router = express.Router();
 
 
 router.get('/swaggerjson', (req, res) => {
-    res.sendFile('swagger.json', { root: './server' });
+    const rootPath = path.resolve("./server");
+    const filePath = path.resolve("./server", "swagger.yaml");    
+    fs.readFile(filePath, {encoding: 'utf8'}, (err, data) => {
+        if (err) {
+            return res.status(404).send();
+        }
+
+        swaggerObject = yaml.load(data);
+
+        res.json(swaggerObject);
+    });
 });
 
 router.get("/", redoc({
